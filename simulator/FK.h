a751f0986f4eff7e9bc16a990da3e242d59176a6
Mark.l <mark_06_20@outlook.com>
2025-10-13 21:55:29 +0200

    IKFK-Model now works

--- simulator/FK.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-// Forward kinematics via Gauss-Newton / Levenberg-Marquardt
-bool fk_iterative_6dof(const float H[6][3], const float p_top[6][3],
-                       const float d_len[6],
-                       float R[9], float T[3],
-                       int iters=25, float lam=1e-3f,
-                       float tol_r=1e-3f, float tol_dx=1e-8f);
